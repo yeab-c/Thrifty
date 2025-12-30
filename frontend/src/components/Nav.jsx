a1e84166e0cec4930050c570b8123ed64f55deea
yeab-c <yeabchanyalew16@gmail.com>
2025-12-30 22:25:42 +0300

    Nav bar file structure for frontend and nav bar

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {assets} from "../assets/assets"
+import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
+const Nav = () => {
+
+    const [visible, setVisible] = React.useState(false);
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+        <img src={assets.logo} className='w-36' alt="" />
+
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+            <NavLink className='flex flex-col items-center gap-1' to="/">
+                <p>HOME</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink className='flex flex-col items-center gap-1' to="/collection">
+                <p>COLLECTION</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink className='flex flex-col items-center gap-1' to="/about">
+                <p>ABOUT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink className='flex flex-col items-center gap-1' to="/contact">
+                <p>CONTACT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+        </ul>
+
+        <div className='flex items-center gap-6'>
+            <img src={assets.search_icon} className='w-5 cursor-pointer' alt="search" />
+
+            <div className='group relative'>
+                <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+                <div className='group-hover:block hidden absolute dropdown right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
+                    </div>
+                </div>
+            </div>
+            <Link to="/cart" className='relative'>
+                <img src={assets.cart_icon} className='w-5 min-w-5' alt="cart" />
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+            </Link>
+            <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+        </div>
+
+        {/* Mobile Menu */}
+        <div className={`absolute top-0 bottom-0 right-0 overflow-hidden bg-white transition-all duration-400 ${visible ? 'w-full' : 'w-0' }`}>
+            <div className='flex flex-col text-gray-700'>
+                <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="" />
+                    <p>Back</p>
+                </div>
+                <NavLink onClick={()=>setVisible(false)} to="/" className='p-4 border-t border-gray-200'>HOME</NavLink>
+                <NavLink onClick={()=>setVisible(false)} to="/collection" className='p-4 border-t border-gray-200'>COLLECTION</NavLink>
+                <NavLink onClick={()=>setVisible(false)} to="/about" className='p-4 border-t border-gray-200'>ABOUT</NavLink>
+                <NavLink onClick={()=>setVisible(false)} to="/contact" className='p-4 border-t border-gray-200'>CONTACT</NavLink>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
