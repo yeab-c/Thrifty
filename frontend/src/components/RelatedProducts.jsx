fa43060a19fe8e56515ee4e787428d2c3a082bb5
yeab-c <yeabchanyalew16@gmail.com>
2026-01-02 21:33:27 +0300

    Product page implemented

---
+++ frontend/src/components/RelatedProducts.jsx
@@ -0,0 +1,37 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/shopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const RelatedProducts = ({category, subCategory}) => {
+
+    const {products} = useContext(ShopContext);
+    const [relatedProducts, setRelatedProducts] = useState([]);
+
+    useEffect(() => {
+
+        if(products.length > 0){
+            let productsCopy = products.slice();
+            productsCopy = productsCopy.filter(item => item.category === category && item.subCategory === subCategory);
+            setRelatedProducts(productsCopy.slice(0,5));
+            
+        }
+
+    }, [products]);
+
+  return (
+    <div className='my-24'>
+        <div className='text-center text-3x1 py-2'>
+            <Title text1={'RELATED'} text2={'PRODUCTS'} />
+        </div>
+
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {relatedProducts.map((item, index) => (
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price}  />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
