b887c4ab3885bde099925b39f1ab90e88ae20e92
yeab-c <yeabchanyalew16@gmail.com>
2026-02-23 23:51:32 +0300

    Implement Contact page with store information, careers section, and newsletter subscription

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-112.5' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>123 Main Street</p>
+          <p className='text-gray-500'>Phone: +251912345678</p>
+          <p className='text-gray-500'>Email: contact@gmail.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Thrifty</p>
+          <p className='text-gray-500'>Learn more about our career opportunities</p>
+          <button className='border border-black px-8 py-4 text-sm cursor-pointer hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+
+      <NewsletterBox />
+    </div>
   )
 }
 
