a38150cd8a5edd30cf7fdf3d6570dde5fc6ac80b
yeab-c <yeabchanyalew16@gmail.com>
2026-02-25 22:34:48 +0300

    Add product and user models with schema definitions

---
+++ backend/models/product.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: Array,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subCategory: {
+        type: String,
+        required: true
+    },
+    sizes: {
+        type: Array,
+        required: true
+    },
+    date: {
+        type: Number,
+        required: true
+    },
+    bestseller: {
+        type: Boolean,
+    }
+})
+
+const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
+
+export default Product;
---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cartData: {
+        type: Object,
+        default: {}
+    }
+}, {minimize: false})
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
