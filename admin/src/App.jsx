6098a1ad1b8826c974407006798544639d82b7d1
yeab-c <yeabchanyalew16@gmail.com>
2026-03-01 17:18:20 +0300

    Add routing for product management pages in the admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Add from './pages/Add'
 
 const App = () => {
   return (
@@ -10,6 +14,13 @@ const App = () => {
         <hr className='border-gray-300' />
         <div className='flex w-full'>
           <Sidebar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path='/add' element={<Add />} />
+              <Route path='/list' element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+            </Routes>
+          </div>
         </div>
       </>
     </div>
