a9321d5b8aeb30cacf9213aa7b0dc59f90edfe2c
yeab-c <yeabchanyalew16@gmail.com>
2026-03-06 21:45:49 +0300

    Implement product listing with data fetching and display

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "ETB";
 
 const App = () => {
 
