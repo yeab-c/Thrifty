3d48dbf589d4c484670f98c37ace47ead38cb4cf
yeab-c <yeabchanyalew16@gmail.com>
2026-03-06 21:56:08 +0300

    Add product removal functionality with token management and list refresh

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { backendUrl, currency } from '../App';
 import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({token}) => {
 
   const [list, setList] = useState([]);
 
@@ -17,6 +17,24 @@ const List = () => {
       } else {
         toast.error(response.data.message);
       }  
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
+  const removeProduct = async (id) => {
+    try {
+      
+      const response = await axios.post(backendUrl + '/api/product/remove', {id}, {headers:{token}});
+
+      if(response.data.success){
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+
     } catch (error) {
       console.log(error);
       toast.error(error.message);
@@ -47,7 +65,7 @@ const List = () => {
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{currency} {item.price}</p>
-              <p className='text-right md:text-center cursor-pointer text-lg'>X</p>
+              <p onClick={()=> removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
             </div>
           ))
         }
